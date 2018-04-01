62675c4bc93a367e2a15f1db47b567ba446b81ba
Alex Harris <akharri8@ncsu.edu>
2018-03-31 21:08:29 -0400

    added mocha, chai, and package.json

---
+++ test/test.js
@@ -0,0 +1,5 @@
+// Mocha and its assertion library are both downloaded
+// Mocha Docs https://mochajs.org/
+// Chai Docs http://www.chaijs.com/
+
+// Each File should have its own testing directory for clear compartmentalization. :^)
