38605064a78f8ff33bccae2fc4a2f74bc7d46bcb
Alex Harris <akharri8@ncsu.edu>
2018-04-01 13:23:31 -0400

    Registration Form JS functionality working

--- public/JavaScript/registrationForm.js
+++ public/JavaScript/registrationForm.js
@@ -1,24 +1,38 @@
-const form = {
-    userInput: function() {
-        // const userName = document.querySelector("#nameInput").value.trim();
-        // const userEmail = document.querySelector("#emailInput").value.trim();
-        // const userPassword = document.querySelector("#passwordInput").value.trim();
-        let userName = 'a';
-        let userEmail = 'b';
-        let userPassword = 'c'
-    
-        const data = {
-            userName,
-            userEmail,
-            userPassword
-        };
-    
-        return data;
-    }
+const button = document.querySelector("#submitButton");
+console.log(button);
+button.addEventListener("click", postUserInput);
+
+function userInput(event) {
+    event.preventDefault();
+    const userName = document.querySelector("#nameInput").value.trim();
+    const userEmail = document.querySelector("#emailInput").value.trim();
+    const userPassword = document.querySelector("#passwordInput").value.trim();
+
+    const data = {
+        userName,
+        userEmail,
+        userPassword
+    };
+
+    return data;
 }
 
 
-// add event listener to button that will get all of our input data.
-// use xhr to send data to server wrapped in an object
-// allow 
-module.exports = form
+function postUserInput() {
+    // Store user Data object to a variable
+    const userData = userInput(event);
+    // Create a new instance of an xmlhttprequest object
+    const xhr = new XMLHttpRequest();
+    // set parameters on method, route, and async
+    xhr.open("POST", "/userData", true);
+    // What happens when we get a response back from server
+    xhr.onload = function() {
+        if (this.status === 200) {
+            console.log(this.responseText);
+        }
+    }
+    // send data
+    xhr.send(userData);
+}
+
+
