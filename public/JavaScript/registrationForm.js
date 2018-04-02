4fca7c4a80ca9dc09ded176ad7a0c708876a29f8
Alex Harris <akharri8@ncsu.edu>
2018-04-02 19:02:34 -0400

    update

--- public/JavaScript/registrationForm.js
+++ public/JavaScript/registrationForm.js
@@ -1,20 +1,95 @@
-const button = document.querySelector("#submitButton");
-button.addEventListener("click", postUserInput);
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDUKEzdKpJQ3aCrue8FDXIHolQV6iQC_EM",
+    authDomain: "project2-d7753.firebaseapp.com",
+    databaseURL: "https://project2-d7753.firebaseio.com",
+    projectId: "project2-d7753",
+    storageBucket: "project2-d7753.appspot.com",
+    messagingSenderId: "92644411279"
+};
+firebase.initializeApp(config);
 
-function userInput(event) {
-    event.preventDefault();
-    const userName = document.querySelector("#nameInput").value.trim();
-    const userEmail = document.querySelector("#emailInput").value.trim();
-    const userPassword = document.querySelector("#passwordInput").value.trim();
 
-    const data = {
-        userName,
-        userEmail,
-        userPassword
-    };
+// DOM hooks
+const loginButton = document.querySelector("#loginButton");
+const signUpButton = document.querySelector("#signUpButton");
+const form = document.querySelector("#registerForm");
 
-    return data;
-}
+// Button created for logged in users
+const signOutButton = document.createElement("button");
+const buttonText = document.createTextNode("Sign Out");
+
+const currentUser = firebase.auth().currentUser;
+
+// This will work for a login button
+loginButton.addEventListener('click', e => {
+    e.preventDefault();
+    const auth = firebase.auth();
+    const email = document.querySelector('#emailInput');
+    const emailVal = email.value.trim();
+    const password = document.querySelector("#passwordInput");
+    const passwordVal = password.value.trim();
+    const promise = auth.signInWithEmailAndPassword(emailVal, passwordVal);
+    promise.catch(e => console.log(e.message));
+
+    email.value = "";
+    password.value = "";
+})
+
+// Sign up function
+signUpButton.addEventListener('click', e => {
+    e.preventDefault();
+    const auth = firebase.auth();
+    const email = document.querySelector('#emailInput').value.trim();
+    const password = document.querySelector("#passwordInput").value.trim();
+
+    const promise = auth.createUserWithEmailAndPassword(email, password);
+    promise.catch(e => console.log(e.message));
+})
+
+signOutButton.addEventListener('click', e => {
+    firebase.auth().signOut();
+});
+
+firebase.auth().onAuthStateChanged(currentUser => {
+    if (currentUser) {
+        console.log(currentUser);
+        // hide the other buttons when a user is logged in
+        loginButton.style.display = "none";
+        signUpButton.style.display = "none";
+        // Append signout button to form
+        signOutButton.classList.add("btn");
+        signOutButton.classList.add("btn-outline-danger");
+        signOutButton.appendChild(buttonText);
+        form.appendChild(signOutButton);
+    } else {
+        console.log("firebaseUser not logged in");
+    }
+});
+
+
+
+
+
+
+
+
+// button.addEventListener("click", postUserInput);
+
+// function userInput(event) {
+//     event.preventDefault();
+//     const userName = document.querySelector("#nameInput").value.trim();
+//     const userEmail = document.querySelector("#emailInput").value.trim();
+//     const userPassword = document.querySelector("#passwordInput").value.trim();
+
+//     const data = {
+//         userName,
+//         userEmail,
+//         userPassword
+//     };
+
+//     return data;
+// }
 
 
 function postUserInput() {
@@ -35,5 +110,4 @@ function postUserInput() {
     // send data
     xhr.send(JSON.stringify(userData));
 }
-
 
