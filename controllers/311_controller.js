19805208e3eb4bdf8e5d0b87039419b9be2402c3
Luke Champ <luke.champ@outlook.com>
2018-04-07 10:44:03 -0400

    building routes

--- controllers/311_controller.js
+++ controllers/311_controller.js
@@ -3,8 +3,51 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 // GET - Registration Form
-router.get("/", (req,res) => res.render('registrationForm'));
+router.get("/register", (req,res) => res.render('registrationForm'));
+
+// GET - Landing Page
+router.get("/", (req,res) => res.render('index'));
+
+// GET - Dashboard
+// router.get("/dashboard", (req,res) => res.render('dashboard'));
+
+
+
+
+// POST-USER TICKET
+router.post("/userTickets", (req,res) => console.log(req.body));
+
+
 // POST - User Data
 router.post("/userData", (req,res) => console.log(req.body));
+
+
+  // PUT route for updating posts
+  app.put("/userTickets", function(req, res) {
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+
+  // DELETE route for deleting tickets
+  app.delete("/userTickets:id", function(req, res) {
+    db.Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+
+
 // Export Router
 module.exports = router;
