8e957cba1d254d2f1f80bfce4cb530ed9becc49b
Maddie <maddiedeming1@gmail.com>
2018-04-12 16:23:51 -0400

    Made it easier on ya

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,16 +1,18 @@
 $.ajax({ url: "/api/tickets", method: "GET" }).done(data => {
     $("#open-dash").on("click", () => {
         $("#mainContent").empty();
-        console.log(data)
         for(var i = 0; i< data.length; i++){
             var card= $("<div>").addClass("card text-white bg-info mb-3")
             var cardHeader = $("<div>").addClass("card-header") 
             var cardBody = $("<div>").addClass("card-body")
             var cardTitle = $("<div>").addClass("card-title")
-            var cardText =$("<p>").addClass("card-text")
-            $(cardText).text(data[i].Answers[i].Question.label + data[i].Answers[i].value);
-            $(cardTitle).text(data[i].status).append(cardText)
-            $(cardBody).append(cardTitle)
+            $(cardTitle).text(data[i].status);
+            $(cardBody).append(cardTitle);
+            for(var x = 0; x < data[i].Answers.length; x++){
+                var cardText =$("<p>").addClass("card-text")
+                $(cardText).html(`${data[i].Answers[x].Question.label}<br>${data[i].Answers[x].value}`);
+                $(cardBody).append(cardText);
+            }
             $(cardHeader).text(data[i].votes).append(cardBody);
             $(card).append(cardHeader);
             $("#mainContent").append(card)
