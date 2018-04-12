d8aece69886d830b75309e76e9dbec5241431f2e
Luke Champ <luke.champ@outlook.com>
2018-04-12 11:56:06 -0400

    merge

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,21 +1,54 @@
-$(document).ready(() => {
-// ON CLICK: Sign-Up
-    $("#sign-up").on("click", () => {
-        $("#login-modal").modal("hide");
-        $("#register-modal").modal("show");
-    });
-// ON CLICK: Login
-    $("#login").on("click", () => {
-        $("#register-modal").modal("hide");
-        $("#login-modal").modal("show");
-    });
-// ON CLICK: Create Ticket
-    $("#createTicket").on('click', () => {
-        $("#ticket-modal").modal("show");
-    });
-// ON CLICK: Reset Password
-    $("#resetPassword").on("click", () => {
-        $("#login-modal").modal("hide");
-        $("#reset-modal").modal("show");
-    });
+
+// var card= $("<div>").addClass("card text-white bg-info mb-3")
+// var cardHeader = $("<div>").addClass("card-header") 
+// var cardBody = $("<div>").addClass("card-body")
+// var cardTitle = $("<div").addClass("card-title")
+// var cardText =$("<p>").addClass("card-text")
+// for (each ticket) {
+// $(cardTitle).text(TicketHeader).append(cardText)
+// $(cardBody).text(TicketInfo).append(cardTitle)
+// $(cardHeader).append(cardBody);
+// $(card).append(cardHeader);
+// $("#mainContent").append(card);}
+
+
+//   // $("#dashboard").onclick(function(){
+//     "#mainContent".empty()
+//       if(ticket is from user){
+//         for (each ticket){
+//           // $(cardTitle).text(TicketHeader).append(cardText)
+// // $(cardBody).text(TicketInfo).append(cardTitle)
+// // $(cardHeader).append(cardBody);
+// // $(card).append(cardHeader);
+//   // $("#mainContent").append(card);}
+//         }
+//       }
+//     }
+//   })
+
+
+$(document).ready(function () {
+  $("#sign-up").on("click", function () {
+    $("#login-modal").modal("hide");
+    $("#register-modal").modal("show");
+  });
+
+  $("#login").on("click", function () {
+    $("#register-modal").modal("hide");
+    $("#login-modal").modal("show");
+  });
+
+  $("#createTicket").on('click', () => {
+    $("#ticket-modal").modal("show");
+  });
+
+  $("#resetPassword").on("click", function () {
+    $("#login-modal").modal("hide");
+    $("#reset-modal").modal("show");
+  });
+
+
+
 });
+
+
