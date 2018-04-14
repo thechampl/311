a13510a4b17f9bb48fdd210104e20be27f5f3582
Alex Harris <akharri8@ncsu.edu>
2018-04-14 00:33:42 -0400

    Altered Email

--- public/assets/js/emailNotifications.js
+++ public/assets/js/emailNotifications.js
@@ -1,5 +1,18 @@
 'use strict';
 const nodemailer = require("nodemailer");
+// const image = require("/assets/img/simpl311.png");
+
+const Module = require('module');
+const fs     = require('fs');
+
+// Module._extensions['.png'] = function(module, fn) {
+//   const base64 = fs.readFileSync(fn).toString('base64');
+//   module._compile('module.exports="data:image/png;base64,' + base64 + '"', fn);
+// };
+
+// var img = require("fs").readFileSync("");
+
+// var image = require("../img/simpl311.png");
 
 module.exports = function (email, user, ticket) {  
   const smtpTransport = nodemailer.createTransport({
@@ -19,7 +32,7 @@ module.exports = function (email, user, ticket) {
       <h3>${ticket}</h3>
       <p>- 311 Adminstration</p>
       `
-  }
+    }
   // Send Email
   smtpTransport.sendMail(mailOptions, (error, response) => {
     // DO SOMETHING
