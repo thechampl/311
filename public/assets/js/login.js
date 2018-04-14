2758abb04243820fe7dc85f2a79b269e8cebcc12
Alex Harris <akharri8@ncsu.edu>
2018-04-14 01:55:30 -0400

    Added validation to log-in

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -8,8 +8,36 @@ loginButton.addEventListener('click', e => {
   e.preventDefault();
   const emailVal = loginEmail.value.trim();
   const passwordVal = loginPassword.value.trim();
-  const promise = auth.signInWithEmailAndPassword(emailVal, passwordVal);
-  promise.catch(e => console.log(e.message));
+  const loginObj = {
+    emailVal,
+    passwordVal
+  }
+  loginValidation(loginObj);
   $("#login-modal").modal("hide");
-  //location.reload();
 });
+
+
+function loginValidation(dataObj) {
+  let breakFlag = null;
+  for (let property in dataObj) {
+    // INVALID: Special Characters
+    if (typeof (dataObj[property]) !== 'object') {
+      if (/[^a-zA-Z0-9@\-\/\s\.]/.test(dataObj[property]) || dataObj[property] === "") {
+        $("#invalid-modal").modal("show");
+        breakFlag = true;
+        break;
+      };
+    }
+    // INVALID: Undefined
+    if (dataObj[property] === "" || undefined) {
+      $("#invalid-modal").modal("show");
+      breakFlag = true;
+      break;
+    };
+  };
+  if (breakFlag === true) {
+    return;
+  };
+  const promise = auth.signInWithEmailAndPassword(dataObj.emailVal, dataObj.passwordVal);
+  promise.catch(e => console.log(e.message));
+};
